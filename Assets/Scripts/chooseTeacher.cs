9fd0defe3eaa414a22aead18185dabee3e382d78
ayazcis <ayazcisem@gmail.com>
2023-04-09 00:56:21 +0300

    [CisemAyaz]start ve secim sahneleri- animasyon

---
+++ Assets/Scripts/chooseTeacher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class chooseTeacher : MonoBehaviour
+{
+    public void omerabi()
+	{
+		//SceneManager.LoadScene(4);
+	}
+	public void atil()
+	{
+		//SceneManager.LoadScene(2);
+	}
+	public void tolgay()
+	{
+		//SceneManager.LoadScene(3);
+	}
+	public void sonat()
+	{
+		//SceneManager.LoadScene(5);
+	}
+}
